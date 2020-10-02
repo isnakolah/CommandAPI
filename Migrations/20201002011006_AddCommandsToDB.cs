a80e40b4ad0f5fe741709f6393ebd17a0934e025
Daniel Nakolah <nakolahdaniel1@gmail.com>
2020-10-02 04:39:53 +0300

    Make migrations and fix the connection string

---
+++ Migrations/20201002011006_AddCommandsToDB.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CommandAPI.Migrations
+{
+    public partial class AddCommandsToDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CommandItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Howto = table.Column<string>(nullable: true),
+                    Platform = table.Column<string>(nullable: true),
+                    Commandline = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CommandItems", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CommandItems");
+        }
+    }
+}
